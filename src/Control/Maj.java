f8d00b67317626f13800015508225f3b2fe73a55
ThomasLrnt <laurent.thomas90@gmail.com>
2019-06-09 16:03:02 +0200

    Ajout de méthodes d'ajout

--- src/Control/Maj.java
+++ src/Control/Maj.java
@@ -24,23 +24,81 @@ public class Maj {
     final String password = "";
     // Objet type connexion -> lien avec la base de donnée
     
-    // Méthodes de Modification d'une année scolaire
+    /** Méthodes de Modification d'une année scolaire
+     * 
+     * @param id
+     * @throws SQLException
+     * @throws ClassNotFoundException 
+     */
     public void ajoutAnnee(int id) throws SQLException, ClassNotFoundException{
         // Objet type connexion -> lien avec la base de donnée
         Connexion maConnexion  = new Connexion(dbName,userName,password);
         // Création de la requete SQL
-        String req = "INSERT INTO annee_scolaire (id) VALUES ("+id+");";
+        String req = "INSERT INTO annee_scolaire ('id') VALUES ("+id+");";
         // Execution de la mise à jour
         maConnexion.executeUpdate(req);
     }
     
-    // Méthodes de Modification d'une Ecole
+    /** Méthodes de Modification d'une Ecole
+     * 
+     * @param id
+     * @param nom
+     * @throws SQLException
+     * @throws ClassNotFoundException 
+     */
+    public void ajoutEcole(int id, String nom) throws SQLException, ClassNotFoundException{
+        // Objet type connexion -> lien avec la base de donnée
+        Connexion maConnexion  = new Connexion(dbName,userName,password);
+        // Création de la requete SQL
+        String req = "INSERT INTO ecole ('id','nom') VALUES ("+id+","+nom+");";
+        // Execution de la mise à jour
+        maConnexion.executeUpdate(req);
+    }
     
-    // Méthodes de Modification d'un Trimestre
+    /** Méthodes de Modification d'un Trimestre
+     * 
+     * @param id
+     * @param num
+     * @param debut
+     * @param fin
+     * @param id_annee
+     * @throws SQLException
+     * @throws ClassNotFoundException 
+     */
+    public void ajoutTrimestre(int id, int num, int debut, int fin, int id_annee) throws SQLException, ClassNotFoundException{
+        // Objet type connexion -> lien avec la base de donnée
+        Connexion maConnexion  = new Connexion(dbName,userName,password);
+        // Création de la requete SQL
+        String req = "INSERT INTO trimestre ('id', 'numero', 'debut', 'fin', 'id_annee_scolaire') VALUES ("+id+","+num+","+debut+","+fin+","+id_annee+");";
+        // Execution de la mise à jour
+        maConnexion.executeUpdate(req);
+    }
     
-    // Méthodes de Modification d'un Niveau
+    /** Méthodes de Modification d'un Niveau
+     * 
+     * @param id
+     * @param nom
+     * @throws SQLException
+     * @throws ClassNotFoundException 
+     */
+    public void ajoutNiveau(int id, String nom) throws SQLException, ClassNotFoundException{
+        // Objet type connexion -> lien avec la base de donnée
+        Connexion maConnexion  = new Connexion(dbName,userName,password);
+        // Création de la requete SQL
+        String req = "INSERT INTO niveau ('id', 'nom') VALUES ("+id+","+nom+");";
+        // Execution de la mise à jour
+        maConnexion.executeUpdate(req);
+    }
     
     // Méthodes de Modification d'une discipline
+    public void ajoutClasse(int id, String nom, int id_ecole, int id_niveau, int id_annee) throws SQLException, ClassNotFoundException{
+        // Objet type connexion -> lien avec la base de donnée
+        Connexion maConnexion  = new Connexion(dbName,userName,password);
+        // Création de la requete SQL
+        String req = "INSERT INTO classe ('id', 'nom', 'id_ecole', 'id_niveau', 'id_annee_scolaire') VALUES ("+ id +","+ nom +","+ id_ecole+","+id_niveau+","+id_annee+");";
+        // Execution de la mise à jour
+        maConnexion.executeUpdate(req);
+    }
     
     // Méthodes de Modification d'une personne
     
